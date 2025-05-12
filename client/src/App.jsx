f87fab5d778f8d126c45b68eb8e89cb9d323cbcc
julianatonu <julianatonu@gmail.com>
2025-05-12 19:52:34 +0600

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 
+import { useLocation } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
+  const isOwnerPath = useLocation().pathname.includes('owner');
   return (
     <div>
-           <h1 className='text-red-600'>Hotel Booking</h1>
+      <h1 className='text-red-600'>Hotel Booking</h1>
+      {!isOwnerPath && <Navbar />}
     </div>
   )
 }
