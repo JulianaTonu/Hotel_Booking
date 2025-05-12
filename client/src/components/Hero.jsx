98ca23d6e34eb30a51ed03639ede21d6f60b0d40
julianatonu <julianatonu@gmail.com>
2025-05-12 20:36:58 +0600

    hero

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='flex flex-col items-start justify-center
+        xl:px-32 text-white bg-[ur("/src/assets/heroImage.png")] h-screen
+        '>
+            
+        </div>
+    );
+};
+
+export default Hero;
