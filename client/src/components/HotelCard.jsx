b2df75f9eb4f45e5bccb94855b3d1185cb45f69e
julianatonu <julianatonu@gmail.com>
2025-05-14 20:50:05 +0600

    hotelcard

--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -28,7 +28,7 @@ const HotelCard = ({ room, index }) => {
 
                 <div className='flex items-center justify-between mt-4'>
                     <p><span className='text-xl text-gray-800'>${room.pricePerNight}</span>/night</p>
-                    <button className='px-4 py-2 text-sm font-medium border border-gray-300 hover:border-gray-50 transition-all cursor-pointer' >Book Now</button>
+                    <button className='px-4 py-2 text-sm font-medium border border-gray-300 hover:border-gray-500 transition-all cursor-pointer hover:text-black' >Book Now</button>
                 </div>
             </div>
            </div>
