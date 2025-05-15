be41a2a1c17f7df8e7fb777d168c4c46fcf702ee
julianatonu <julianatonu@gmail.com>
2025-05-16 00:25:22 +0600

    filters

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets, facilityIcons, roomsDummyData } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
 import StarRating from '../components/StarRating';
 
+const CheckBox = ({ label, selected = false, onChange = () => { } }) => {
+    return (
+        <label className='flex gap-3 items-center cursor-pointer mt-2 text-sm'>
+            <input type="checkbox" checked={selected} 
+            onChange={(e)=>onChange(e.target.checked, label)}
+             />
+
+             <span className='font-light select-none'>{label}</span>
+        </label>
+    )
+}
+
+const RadioButton = ({ label, selected = false, onChange = () => { } }) => {
+    return (
+        <label className='flex gap-3 items-center cursor-pointer mt-2 text-sm'>
+            <input type="radio" name='sortOption' checked={selected} 
+            onChange={()=>onChange(label)}
+             />
+             <span className='font-light select-none'>{label}</span>
+        </label>
+    )
+}
+
 const AllRooms = () => {
+
+    const [openFilters, setOpenFilters] = useState(false)
     const navigate = useNavigate()
+
+    const roomTypes = [
+        "Single Bed",
+        "Family Suite",
+        "Double Bed",
+        "Luxury Room"
+    ];
+
+    const priceRanges = [
+        "0 to 500",
+        "500 to 1000",
+        "1000 to 2000 ",
+        "2000 to 3000 "
+    ];
+    const sortOptions = [
+        "Price Low to High",
+        "Price high to Low",
+        "Newest First",
+    ];
+
     return (
         <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 md:px-16
          lg:px-24 xl:px-32'>
@@ -17,13 +62,13 @@ const AllRooms = () => {
                 </div>
                 <div className=''>
                     {roomsDummyData.map((room) => (
-                        <div key={room._id} className='flex gap-3 my-6'>
+                        <div key={room._id} className='md:flex md:justify-center px-2  gap-3 my-6'>
                             <img onClick={() => { navigate(`/rooms/${room._id}`); scrollTo(0, 0) }} src={room.images[0]} alt="hotel-img"
                                 className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover
                             cursor-pointer'
                             />
                             <div className='md:w-1/2 flex flex-col gap-2'>
-                                <p className='text-gray-500'>{room.hotel.city}</p>
+                                <p className='text-gray-500 mt-2'>{room.hotel.city}</p>
                                 <p>{room.hotel.name}</p>
                                 <div className='flex items-center'>
                                     <StarRating />
@@ -36,7 +81,7 @@ const AllRooms = () => {
                                     <p className='text-gray-500 pl-0.5'>{room.hotel.address}</p>
                                 </div>
                                 {/* {Room Amenities } */}
-                                <div className='flex items-center mt-3 mb-6 gap-4'>
+                                <div className='flex items-center mt-3 mb-4 gap-4'>
                                     {room.amenities.map((item, index) => (
                                         <div key={index} className='flex items-center gap-2 px-2 py-2 rounded-lg bg-[#ebebed] '>
                                             <img src={facilityIcons[item]} alt={item} className='w-4 h-4' />
@@ -46,13 +91,55 @@ const AllRooms = () => {
 
                                 </div>
                                 {/* Room Price per night / */}
-                                <p className='text-xl font-medium text-gray-700'>${room.pricePerNight}</p>
+                                <p className='text-xl font-medium text-gray-700'>${room.pricePerNight}/ night</p>
                             </div>
                         </div>
                     ))}
 
                 </div>
             </div>
+
+            {/* {filters } */}
+            <div className='bg-white w-80 border border-gray-800 text-gray-600 max-lg:mb-8 sm:ml-6 md:ml-0 min-lg:mt-16'>
+                <div className='flex justify-between px-4 py-2'>
+                    <p className='text-base font-medium'>FILTERS</p>
+                    <div className='text-xs cursor-pointer'>
+                        <span onClick={() => setOpenFilters(!openFilters)}
+                            className='lg:hidden'>
+                            {openFilters ? 'HIDE' : 'SHOW'}
+                        </span>
+                        <span className='hidden lg:block'>CLEAR</span>
+
+                    </div>
+                </div>
+
+                <div className={`${openFilters ? 'h-auto' : 'h-0 lg:h-auto'}
+                overflow-hidden transition-all duration-700`}>
+                    
+                    <div className='px-5 pt-5'>
+                        <p className='font-medium text-gray-800 pb-2'>Popular filters</p>
+                        {roomTypes.map((room, index)=>(
+                            <CheckBox key={index} label={room}/>
+                        ))}
+                    </div>
+
+                    <div className='px-5 pt-5'>
+                        <p className='font-medium text-gray-800 pb-2'>Price Ranges </p>
+                        {priceRanges.map((range, index)=>(
+                            <RadioButton key={index} label={`$ ${range}`}/>
+                        ))}
+                    </div>
+
+                        <div className='px-5 pt-5 mb-2'>
+                        <p className='font-medium text-gray-800 pb-2'>Sort By</p>
+                        {sortOptions.map((room, index)=>(
+                            <RadioButton key={index} label={room}/>
+                        ))}
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     );
 };
