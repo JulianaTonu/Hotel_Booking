c431adce7a8da6b76972e3d0340b8c8d24c1134d
julianatonu <julianatonu@gmail.com>
2025-05-16 17:06:10 +0600

    RoomHighlights

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { assets, roomsDummyData } from '../assets/assets';
+import { assets, facilityIcons, roomsDummyData } from '../assets/assets';
 import StarRating from '../components/StarRating';
 
 const RoomDetails = () => {
@@ -33,7 +33,7 @@ const RoomDetails = () => {
             {/* {hotel address } */}
             <div className='flex text-gray-500 py-2'>
                 <img src={assets.locationIcon} alt="" />
-                <p className='font-inter text-sm pl-1'>{room.address}</p>
+                <p className='font-inter text-sm pl-1'>{room.hotel.address}</p>
             </div>
 
             {/* {Room Image } */}
@@ -53,6 +53,22 @@ const RoomDetails = () => {
 
                     ))}
                 </div>
+            </div>
+
+            {/* {Room Highlights } */}
+            <div className='flex flex-col md:flex-row md:justify-between mt-10'>
+                <div className='  '>
+                    <h1 className='text-3xl md:text-4xl font-playfair'>Experience Luxury Like Never before</h1>
+                    <div className='flex gap-3 mt-6 '>
+                        {room.amenities.map((item, index) => (
+                            <div className='flex items-center gap-2 px-3 py-2 rounded-lg bg-gray-200' key={index}>
+                                <img src={facilityIcons[item]} alt="item" className='w-5 h-5' />
+                                <p>{item}</p>
+                            </div>                           
+                        ))}
+                    </div>
+                </div>
+                    <p className='font-medium text-2xl'>${room.pricePerNight}/night</p>
             </div>
         </div>
     );
