809788113428b36c6c8a1ce17be7fd2afea0dfc2
julianatonu <julianatonu@gmail.com>
2025-05-16 16:52:43 +0600

    roomDetails

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/rooms' element={<AllRooms/>}/>
+          <Route path='/rooms/:id' element={<RoomDetails/>}/>
         </Routes>
       </div>
       <Footer/>
