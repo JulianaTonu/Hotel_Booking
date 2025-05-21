2a0dc88da8af86cfaadd932efe79d577c15f7286
julianatonu <julianatonu@gmail.com>
2025-05-21 16:17:19 +0600

    Addroom

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -2,9 +2,28 @@ import React, { useState } from 'react';
 import Title from '../../components/Title';
 
 const AddRoom = () => {
-  
+    const [image, setImage] =useState({
+        1:null,
+        2:null,
+        3:null,
+        4:null
+    })
+    const [inputs, setInputs] =useState({
+        roomType: '',
+        pricePerNight:0,
+        amenities:{
+            'Free Wifi' : false,
+            'Free Breakfast' : false,
+            'Free Service' : false,
+            'Mountain View' : false,
+            'Pool access' : false,
+        }
+    })
     return (
-      
+        <form>
+            <Title align='left' font='outfit' title='Add Room' subTitle='Fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience'/>
+            <p>Images</p>
+        </form>
     );
 };
 
