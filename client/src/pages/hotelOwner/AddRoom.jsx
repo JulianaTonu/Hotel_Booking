9ff299580e0cdeb0e64e1103a23c67997ccef3fe
julianatonu <julianatonu@gmail.com>
2025-05-21 16:16:23 +0600

    dashboard

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Title from '../../components/Title';
 
 const AddRoom = () => {
+  
     return (
-        <div>
-            
-        </div>
+      
     );
 };
 
