e1f65ebc59f6ef4385d18f2086eac6f801d54536
julianatonu <julianatonu@gmail.com>
2025-05-26 15:40:26 +0600

    clerk

--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ import express from 'express';
 import "dotenv/config";
 import cors from "cors";
 import connectDB from './controllers/db.js';
+import { clerkMiddleware } from 'clerk/express';
+
 
 connectDB()
 
 const app = express()
 app.use(cors()) // Enable Cross-Origin Resource sharing
 
-app.get('/', (req, res)=>res.send("Tonu's API is working"))
+// Middleware 
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
+app.get('/', (req, res) => res.send("Tonu's API is working"))
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`)) 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
