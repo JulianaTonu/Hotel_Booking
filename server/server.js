d1c85da984882abbf59bf0026a710e59d21de2ca
julianatonu <julianatonu@gmail.com>
2025-05-27 10:58:20 +0600

    clerk

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import cors from "cors";
 import connectDB from './configs/db.js';
-import { clerkMiddleware } from 'clerk/express';
+import { clerkMiddleware } from '@clerk/express';
 
 
 connectDB()
@@ -19,4 +19,4 @@ app.get('/', (req, res) => res.send("Tonu's API is working"))
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))  
