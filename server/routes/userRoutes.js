6dce7c03b844c065025d42af81b3f7575862ada1
julianatonu <julianatonu@gmail.com>
2025-05-29 11:35:41 +0600

    hotel

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
-import { getUserdata } from "../controllers/userController.js"
+import { getUserdata, storeRecentSeachedCities } from "../controllers/userController.js"
 import express from 'express'
 import { protect } from "../middleware/authMiddleware.js"
 
 const userRouter = express.Router()
 
 userRouter.get('/', protect, getUserdata)
+userRouter.post('/store-recent-search', protect, storeRecentSeachedCities)
 
 export default userRouter
