050c70b9259374f4d39be9749efc4895d61ced98
julianatonu <julianatonu@gmail.com>
2025-06-18 11:59:42 +0600

    checkAvailability

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,13 @@
+const checkAvailability =async ({checkInDate, checkOutDate, room})=>{
+    try {
+        const bookings =await Booking.find({
+            room,
+            checkInDate:{$lte: checkOutDate},
+            checkOutDate:{$gte: checkInDate}
+        });
+        const isAvailable = bookings.length === 0;
+        return isAvailable;
+    } catch (error) {
+        console.error(error.message)
+    }
+}
