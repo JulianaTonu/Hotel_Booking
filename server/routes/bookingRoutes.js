c40b36afe298b735522ef3250486c6c89e0cc0a1
julianatonu <julianatonu@gmail.com>
2025-06-19 15:50:26 +0600

    bookingRoutes

---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { checkAvailabilityAPI, createBooking, getHotelBookings, getUserBookings } from "../controllers/bookingController";
+import { protect } from "../middleware/authMiddleware";
+
+
+const bookingRouter = express.Router();
+
+bookingRouter.post('/check-availability', checkAvailabilityAPI);
+bookingRouter.post('/book', protect, createBooking);
+bookingRouter.get('/user', protect, getUserBookings);
+bookingRouter.get('/user', protect, getHotelBookings);
+
+
+export default bookingRouter
